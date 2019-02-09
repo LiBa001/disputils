9f7dfc775473d003640002198af9481f9c0521a0
Linus Bartsch <frenki57a@gmail.com>
2019-02-09 21:49:55 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='disputils',
+    version='0.1.1',
+    description='Some utilities for discord.py. Making Discord bot development easier.',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/LiBa001/disputils',
+    author='Linus Bartsch',
+    author_email='linus.pypi@mabasoft.de',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+    ],
+    python_requires='>=3.6',
+    keywords='discord discord-py discord-bot utils utility',
+    packages=find_packages(exclude=['tests', 'docs']),
+    data_files=None
+)
