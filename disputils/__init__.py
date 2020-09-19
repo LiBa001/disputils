766261687d25a4b0ef31ce99d2f31791bbe72c28
yonderbread <realmctbf@gmail.com>
2020-09-19 06:48:29 +0000

    Mass messages delete utility

--- disputils/__init__.py
+++ disputils/__init__.py
@@ -2,3 +2,6 @@ from .pagination import EmbedPaginator, BotEmbedPaginator
 from .confirmation import Confirmation, BotConfirmation
 from .multiple_choice import MultipleChoice, BotMultipleChoice
 from . import abc
+
+# Utility functions (more to come soon)
+from .utils import mass_purge
---
+++ disputils/utils.py
@@ -0,0 +1,22 @@
+async def mass_purge(channel, limit: int, inclusive=False):
+    amount = abs(limit)
+    if amount > 100:
+        iterations, rem = divmod(amount, 100)
+        if inclusive:
+            for i in range(iterations):
+                await channel.purge(limit=100)
+            await channel.purge(limit=rem)
+            return
+        else:
+            for i in range(iterations):
+                messages = channel.history(limit=100).flatten()
+                for message in range(0, len(messages)):
+                    if message != 0:
+                        await messages[message].delete()
+            async for message in channel.history(limit=rem):
+                await message.delete()
+    else:
+        messages = channel.history(limit=amount).flatten()
+        for message in range(0, len(messages)):
+            if inclusive and message != 0:
+                await messages[message].delete()
