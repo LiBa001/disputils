f8008be93e41f2a79306eb9d8b9bc7db7830c288
ZincXX <purnesh_s@yahoo.com>
2020-12-12 18:07:16 +0000

    Update setup.py
    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r', encoding='utf-8') as f:
 
 setup(
     name='disputils',
-    version='0.1.3',
+    version='0.1.4',
     description='Some utilities for discord.py. Making Discord bot development easier.',
     long_description=readme,
     long_description_content_type='text/x-rst',
