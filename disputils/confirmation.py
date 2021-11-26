d963dbeb1a6505cffdce5eca57ea7ac7bf6500b1
Zaeem Technical <duranizaeem@gmail.com>
2021-11-26 23:15:19 +0530

    Update confirmation.py

--- disputils/confirmation.py
+++ disputils/confirmation.py
@@ -64,7 +64,7 @@ class Confirmation(Dialog):
 
         emb = discord.Embed(title=text, color=self.color)
         if not hide_author:
-            emb.set_author(name=str(user), icon_url=user.avatar_url)
+            emb.set_author(name=str(user), icon_url=user.avatar.url)
 
         self._embed = emb
 
